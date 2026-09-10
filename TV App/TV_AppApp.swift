43bcdfad0343cbb240b460a91a5552cf2ff14f52
wikosac <wesibrani@gmail.com>
2026-09-10 10:28:12 +0700

    Initial Commit

---
+++ TV App/TV_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_AppApp.swift
+//  TV App
+//
+//  Created by Dwiko Indrawansyah on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TV_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
